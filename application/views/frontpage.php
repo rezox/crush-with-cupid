79aaf111d05a50ab6e58447ef0d867de37d93b44
Steven Lu <tacticalazn@gmail.com>
2013-02-07 16:55:42 -0500

    Fixing temp styles.

--- application/views/frontpage.php
+++ application/views/frontpage.php
@@ -2,18 +2,6 @@
 	<div class="container">
 		<div id="center">
 			<h1>Crush With Friends</h1>
-			<p>
-				How-to: Crush With Friends
-				<ol>
-					<li>Select your Crushes</li>
-					<li>We'll send an anonymous message that one of their friends is crushin' on them.</li>
-					<li>They'll only see if they're crushin' too!</li>
-				</ol>
-
-				They rest is up to you two -- bang if you want, hang if you don't.<br /><br />
-
-				Get Crushin'
-			</p>
 
 			<div id="login-bar">
 				<a id="fb-login" class="btn btn-large" href="<?= $facebook_auth_url ?>"><img src="assets/img/f_logo.png" ?>Login with Facebook</a>
