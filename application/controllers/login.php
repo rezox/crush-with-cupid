eed6448f383e979fdd9f1ace411665f71f9ba658
Steven Lu <tacticalazn@gmail.com>
2013-02-07 14:47:34 -0500

    Fixing some bugs.

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -6,7 +6,6 @@ class Login extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->library('session');
 		$this->load->library('fb');
 	}
 
@@ -18,8 +17,8 @@ class Login extends CI_Controller
 		$user = $this->fb->get_user();
 
 		$this->load->model('users_model');
-		if (!$user = $this->users_model->find_by_fbid($user['uid']))
-			$user = $this->users_model->add($user);
+		if (!$this->users_model->find_by_fbid($user['uid']))
+			$this->users_model->add($user);
 
 		return redirect('/choose', 'refresh');
 	}
