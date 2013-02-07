34541525c77c00ccd6807f06cadc6e0838180619
Steven Lu <tacticalazn@gmail.com>
2013-02-07 11:34:17 -0500

    Changing terminology.

--- application/config/fb.php
+++ application/config/fb.php
@@ -5,7 +5,7 @@ if (ENVIRONMENT == 'development')
 	$config['fb_appid'] = '257690257696643';
 	$config['fb_secret'] = 'c6880f32c33d0939ca477615f6d00b20';
 }
-else if (ENVIRONMENT == 'staging')
+else if (ENVIRONMENT == 'testing')
 {
 	$config['fb_appid'] = '458845597504327';
 	$config['fb_secret'] = 'c24e68ab407f0ada8897d2c142b78b8b';
