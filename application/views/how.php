bd10cad64353d352ea9e1240388ef95788afd76a
Steven Lu <tacticalazn@gmail.com>
2013-02-11 23:05:04 -0500

    Added how page.

---
+++ application/views/how.php
@@ -0,0 +1,46 @@
+<div id="how" class="container">
+	<div id="content">
+		<div id="heading">
+			<h1>How we do it</h1>
+			<p>Asking someone out is rough. Too rough. Crush With Cupid here to help.</p>
+		</div>
+		<div class="row" id="steps">
+			<div id="step1" class="step span6">
+				<div class="inner">
+					<div class="step-num">1</div>
+					<div class="image"><img id="sample" src="<?= base_url('assets/img/sample.png') ?>" /></div>
+					<div class="text">Log in with Facebook and select the people you're crushin' on.</div>
+				</div>
+			</div>
+			<div id="step2" class="step span6">
+				<div class="inner">
+					<div class="step-num">2</div>
+					<div class="image"><img id="arrow" src="<?= base_url('assets/img/arrow.png') ?>" /></div>
+					<div class="text">We'll send anonymous emails to your crushes telling them that someone has a crush on them.</div>
+				</div>
+			</div>
+			<div id="step3" class="step span6">
+				<div class="inner">
+					<div class="step-num">3</div>
+					<div class="image"><img id="email" src="<?= base_url('assets/img/email.png') ?>" /></div>
+					<div class="text">After they log on and pick their crushes, we'll notify both of you if you're both crushin' on each other!</div>
+				</div>
+			</div>
+			<div id="step4" class="step span6">
+				<div class="inner">
+					<div class="step-num">4</div>
+					<div class="image"><img id="martini" src="<?= base_url('assets/img/martini.png') ?>" /></div>
+					<div class="text">If there's a match, receive exclusive deals (powered by Groupon) for your future date!</div>
+				</div>
+			</div>
+		</div>
+		<div id="bottom">
+			<h1>Good to know</h1>
+			<ul>
+				<li>&bull; We will <span class="highlight">never</span>, ever tell your crush your name unless they have a crush on you too!</li>
+				<li>&bull; We will <span class="highlight">never</span> post on your wall, or anyone else's wall that you have a crush.</li>
+				<li>&bull; You can have as many crushes as you want.</li>
+			</ul>
+		</div>
+	</div>
+</div>
