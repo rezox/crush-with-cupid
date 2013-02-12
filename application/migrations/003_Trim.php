3c27ee4eb643013854c6198e4b69c48ffbe12c88
Steven Lu <tacticalazn@gmail.com>
2013-02-12 11:55:25 -0500

    Trimming away from the database.

---
+++ application/migrations/003_Trim.php
@@ -0,0 +1,22 @@
+<?php
+
+class Trim_crushes extends CI_Migration
+{
+
+	function up()
+	{
+		$this->dbforge->drop_column('users', 'pic_square');
+	}
+
+	function down()
+	{
+		$fields = array(
+			'pic_square' => array(
+				'type' => 'TEXT'
+			),
+		);
+
+		$this->dbforge->add_column('users', $fields);
+	}
+
+}
