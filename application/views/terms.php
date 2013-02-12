6fe95bcf7309fdae59133e10bf175ead59ee51e2
Steven Lu <tacticalazn@gmail.com>
2013-02-11 21:04:40 -0500

    Added terms and privacy.

---
+++ application/views/terms.php
@@ -0,0 +1,57 @@
+<div class="container">
+
+	<h3>Web Site Terms and Conditions of Use</h3>
+
+	<ol>
+		<li>
+			<h5>Terms</h5>
+			<p>By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this web site are protected by applicable copyright and trade mark law.</p>
+		</li>
+
+		<li>
+			<h5>Use License</h5>
+			<p>Permission is granted to temporarily download one copy of the materials (information or software) on Crush With Cupid's web site for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:</p>
+			<ul>
+				<li>modify or copy the materials;</li>
+				<li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
+				<li>attempt to decompile or reverse engineer any software contained on Crush With Cupid's web site;</li>
+				<li>remove any copyright or other proprietary notations from the materials; or</li>
+				<li>transfer the materials to another person or "mirror" the materials on any other server.</li>
+				<li>This license shall automatically terminate if you violate any of these restrictions and may be terminated by Crush With Cupid at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</li>
+			</ul>
+
+		<li>
+			<h5>Disclaimer</h5>
+			<p>The materials on Crush With Cupid's web site are provided "as is". Crush With Cupid makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, Crush With Cupid does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.</p>
+		</li>
+
+		<li>
+			<h5>Limitations</h5>
+			<p>In no event shall Crush With Cupid or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on Crush With Cupid's Internet site, even if Crush With Cupid or a Crush With Cupid authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.</p>
+
+		<li>
+			<h5>Revisions and Errata</h5>
+			<p>The materials appearing on Crush With Cupid's web site could include technical, typographical, or photographic errors. Crush With Cupid does not warrant that any of the materials on its web site are accurate, complete, or current. Crush With Cupid may make changes to the materials contained on its web site at any time without notice. Crush With Cupid does not, however, make any commitment to update the materials.</p>
+		</li>
+
+		<li>
+			<h5>Links</h5>
+			<p>Crush With Cupid has not reviewed all of the sites linked to its Internet web site and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Crush With Cupid of the site. Use of any such linked web site is at the user's own risk.</p>
+		</li>
+
+		<li>
+			<h5>Site Terms of Use Modifications</h5>
+			<p>Crush With Cupid may revise these terms of use for its web site at any time without notice. By using this web site you are agreeing to be bound by the then current version of these Terms and Conditions of Use.</p>
+
+		<li>
+			<h5>Governing Law</h5>
+			<p>Any claim relating to Crush With Cupid's web site shall be governed by the laws of the State of New York without regard to its conflict of law provisions.</p>
+			<p>General Terms and Conditions applicable to Use of a Web Site.</p>
+		</li>
+
+	<ol>
+
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+
+</div>
