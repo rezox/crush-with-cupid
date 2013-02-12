74b6d74dfdd932add980ba0a27cc00d98f510624
Steven Lu <tacticalazn@gmail.com>
2013-02-12 01:35:05 -0500

    Adding Google Analytics.

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -24,6 +24,19 @@
 		js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
 		ref.parentNode.insertBefore(js, ref);
 	}(document, /*debug*/ false));
+
+
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-38420653-1']);
+	_gaq.push(['_setDomainName', 'crushwithcupid.com']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+
 </script>
 </html>
 <!-- Built by Steven Lu (sjlu at me dot com) -->
