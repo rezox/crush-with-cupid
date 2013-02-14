dc0eb2a938f9fe11f12b42a89293bf19237fd335
Steven Lu <tacticalazn@gmail.com>
2013-02-13 22:55:06 -0500

    Special case senario if Facebook effs up logins.

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -16,6 +16,8 @@ class Login extends CI_Controller
 			return redirect('/', 'refresh');
 
 		$user = $this->fb->get_user();
+      if (!isset($user['uid']) || empty($user['uid']))
+         redirect('/logout', 'refresh');
 
 		$this->load->model('users_model');
 		if (!$this->users_model->find($user['uid']))
