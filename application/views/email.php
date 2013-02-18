8b9a0df0153e91be1166b40453ad284468005a1c
Steven Lu <tacticalazn@gmail.com>
2013-02-18 13:55:37 -0500

    Fixed up email copy.

--- application/views/email.php
+++ application/views/email.php
@@ -48,7 +48,7 @@
 																Could it be, love at last? Yes, yes it is.<br />
 																Your crush <span style="font-size: 18px; font-weight: bold;"><?= $name ?></span> is crushin' on you too!<br />
 																<br />
-                                                                We are estaic to bring you two sexy thangs together.<br />
+                                                                We are ecstatic to bring you two sexy thangs together.<br />
                                                                 Now go, be young and have fun!<br />
                                                                 <br />
                                                                 This love letter brought to you by <a href="<?= site_url() ?>" style="color: #336699; font-weight: normal; text-decoration: underline;">CrushWithCupid.com</a><br />
